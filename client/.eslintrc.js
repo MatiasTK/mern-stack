a5a5de3a0fd78376b36bbea00466112d9c95a905
MatiasTK <matiavallejo@gmail.com>
2022-06-11 22:25:52 -0300

    fix: rules

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -18,8 +18,14 @@ module.exports = {
     'react',
   ],
   rules: {
-    'react/jsx-filename-extension': 'off',
+    'react/jsx-filename-extension': 'off', /* To fix import problems */
     'react/react-in-jsx-scope': 'off',
-    'import/extensions': 'off',
+    'import/extensions': 'off', /* Because of react unused imports */
+    'no-underscore-dangle': 'off',
+    'jsx-a11y/label-has-associated-control': ['error', {
+      required: {
+        some: ['nesting', 'id'],
+      },
+    }],
   },
 };
