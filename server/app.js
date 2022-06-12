6fd133029f1b67c8a6a1d2fc8b8360efd0d69144
MatiasTK <matiavallejo@gmail.com>
2022-06-12 01:19:21 -0300

    feat: prepare to deploy to heroku

--- server/app.js
+++ server/app.js
@@ -1,17 +1,24 @@
+/* eslint-disable no-underscore-dangle */
 import express from 'express';
 import fileupload from 'express-fileupload';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
 import postRoutes from './routes/posts.routes.js';
 import { PORT } from './config.js';
 
 export default function startExpress() {
   const app = express();
+  // Manual __dir
+  const __dirname = dirname(fileURLToPath(import.meta.url));
 
   app.use(express.json());
   app.use(fileupload({ useTempFiles: true, tempFileDir: './uploads' }));
 
   app.use(postRoutes);
 
-  app.listen(PORT || 5000, () => {
-    console.log(`Server is running on port: ${PORT || 5000}`);
+  app.use(express.static(join(__dirname, '../client/build')));
+
+  app.listen(PORT || 4000, () => {
+    console.log(`Server is running on port: ${PORT || 4000}`);
   });
 }
--- server/config.js
+++ server/config.js
@@ -2,6 +2,6 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const { PORT } = process.env;
+export const { PORT } = process.env || 4000;
 export const DBURI = process.env.MONGO_URI;
 export const { IMGUR_KEY } = process.env;
